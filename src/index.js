c1a3b812c8a4936b280db7ce7614a4218fb5ad4a
eldr0n-git <eldron@gmail.com>
2021-08-18 17:19:39 +0600

    fix calculations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+    let stringValue = Math.abs(n).toString();
+    let newNumber = '';
+    let j = 0;
   
-}
+    for (let i = stringValue.length-1; i >= 0; i--){
+      newNumber = newNumber + stringValue[i].toString();
+      j++;
+    }
+  
+    return newNumber;
+  }
+  
